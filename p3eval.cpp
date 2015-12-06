ec1ea3734153d3a5a01d92038b554ed82d047eaa
taylork2 <taylork2@users.noreply.github.com>
2015-12-06 01:28:58 -0500

    Correct project 3

---
+++ p3eval.cpp
@@ -0,0 +1,61 @@
+//
+//  p3eval
+//
+
+// these are stubs for assignment 3
+
+#include "p3parser.h"
+
+Value *
+PTree::eval()
+{
+    return 0;
+}
+
+Value *
+PTreeStmtList::eval()
+{
+    return 0;
+}
+
+Value *
+PTreeSet::eval()
+{
+    return 0;
+}
+
+Value *
+PTreePrint::eval()
+{
+    return 0;
+}
+
+Value *
+PTreeUnion::eval()
+{
+    return 0;
+}
+
+Value *
+PTreeIntersect::eval()
+{
+    return 0;
+}
+
+Value *
+PTreeID::eval()
+{
+    return 0;
+}
+
+Value *
+PTreeSTR::eval()
+{
+    return 0;
+}
+
+Value *
+PTreeSearch::eval()
+{
+    return 0;
+}
